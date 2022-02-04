1235af6d3ff8e1eb39edf323f277517e45fc01b0
James Dingwall <james@dingwall.me.uk>
2022-02-04 14:45:20 +0000

    Start 0.43pre

--- autolog.c
+++ autolog.c
@@ -1,6 +1,6 @@
 /**
  * SPDX-License-Identifier: GPL-2.0-or-later
- * version 0.42.1: James Dingwall <james.dingwall@ncrvoyix.com>
+ * version 0.43pre: James Dingwall <james.dingwall@ncrvoyix.com>
  */
 
 /*****************************************************************************/
