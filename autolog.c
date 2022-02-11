7a4299452287149f5dcdb4f9457c64f7bed97563
James Dingwall <james@dingwall.me.uk>
2022-02-11 13:22:31 +0000

    Add LICENSE (GPL v2) file to repository
    Replace the inline GPL text with a LICENSE file and SPDX header.

--- autolog.c
+++ autolog.c
@@ -1,4 +1,5 @@
 /**
+ * SPDX-License-Identifier: GPL-2.0-or-later
  * version 0.42.1: James Dingwall <james.dingwall@ncrvoyix.com>
  */
 
@@ -39,20 +40,6 @@
     Modified		  by Michael C. Mitchell, 15Oct94
     Rewritten		  by Kyle Bateman,	  Nov94, Aug95
     Adapted to my System, by Carsten Juerges,	  Apr 2000
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #include    <stdio.h>
